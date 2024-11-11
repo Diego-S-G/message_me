8439e6c19fb97afcc86910cf61b0a5d31aebd79b
Diego-S-G <diego07goncalves@gmail.com>
2024-11-11 14:56:16 -0300

    Implementa métodos create e destroy na sessão do usuário.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'Usuário logado com sucesso!'
+      redirect_to root_path
+    else
+      flash.now[:error] = 'Algo de errado aconteceu com seu Login'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'Usuário desconectado.'
+    redirect_to login_path
+  end
 end
